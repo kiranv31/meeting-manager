b19ae45ce2fe17f26c7752cc9b48c53011ea692e
Kiran <kiran.verma.cs@gmail.com>
2020-05-26 16:01:32 +0530

    Skeleton for meeting app

---
+++ src/test/java/com/resourcebooking/meetingrooms/MeetingRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourcebooking.meetingrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
